37a23bd83f04a2ee70f6119c6d5149b61ee7f20b
mohammedaminechaieb <mohammedamine.chaieb@ensi-uma.tn>
2026-01-21 11:28:27 +0100

    Update ctf.sql

--- backend/ctf.sql
+++ backend/ctf.sql
@@ -33,9 +33,10 @@ CREATE TABLE challenges (
 
 INSERT INTO challenges (name, category, description, resource_url, flag) VALUES
 ('Gate 1 – Are You Even Here?', 'Forensics', 'Before going deeper...', 'downloads/gate1.zip', 'TUNIHACK{JUST-TaS5IN-0U_B4RRA}'),
-('Cleartext Credentials', 'Networks', 'Someone really logged in...', 'downloads/gate2.zip', 'TUNIHACK{HTTP_IS_NOT_SECURE}'),
+('Cleartext Credentials', 'Networks', 'Someone really logged in...', 'downloads/gate2.zip', 'TUNIHACK{PCAP_HTTP_IS_DANGEROUS}'),
 ('Gate 3', 'Crypto', 'manouba is such a color...', 'downloads/gate3.zip', 'TUNIHACK{RAZI_NEVER_WAS_CALM}'),
 ('Hidden Nmap Analysis', 'Forensics', 'The key is: nmap', 'downloads/gate4.zip', 'TUNIHACK{NMAP_IS_NOT_JUST_PORTS}'),
 ('Gate 5', 'Reverse', 'dragon', 'downloads/gate5.zip', 'TUNIHACK{EJ4AW_TAWA_BD4}'),
 ('Gate 6', 'Reverse', 'We lost the flag—but not really...', 'downloads/gate6.zip', 'TUNIHACK{EJ4AW_TAWA_BD4_2}'),
 ('Gate 7', 'Only for smart ones', 'Just answer', 'downloads/gate7.zip', 'TUNIHACK{RAFEH}');
+
